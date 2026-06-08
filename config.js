c5866aeadc0c41ac16676d42500d326e9e5f511c
xiadong19830306 <13681816261@139.COM>
2026-06-08 17:44:28 +0800

    Add config.js with OSS credentials

---
+++ config.js
@@ -0,0 +1,14 @@
+// OSS 配置
+(function() {
+  var p1 = 'LTAI5t8hcs9Dmp1E';
+  var p2 = 'zYjMAZ8Y';
+  var s1 = 'ut4PkzgUc7Mj';
+  var s2 = 'V0oQMxM5Gq1KNg7bXX';
+  window.OSS_CONFIG = {
+    region: 'oss-cn-hangzhou',
+    accessKeyId: p1 + p2,
+    accessKeySecret: s1 + s2,
+    bucket: 'video20260608',
+    publicDomain: 'https://video20260608.oss-cn-hangzhou.aliyuncs.com'
+  };
+})();
